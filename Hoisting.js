665d9c7a14c9695e3bf8611bb0f68846d146e4be
NandhiniNarayanasamy <nandininarayanasamy1997@gmail.com>
2025-01-03 22:15:35 +0530

    regarding Hosting and function

---
+++ Hoisting.js
@@ -0,0 +1,29 @@
+// //Hoisting Variables
+// var acc_name="nandhu";
+// console.log(acc_name)
+// var bank_name="SBI"
+// console.log(bank_name)
+//Hoisting function
+// sayMyName()
+// var acc_name="nandhu"
+// function sayMyName(){
+//     let name="sam";
+//     console.log("Name:",name)
+// }
+// Arrow function
+// let arr_func=()=>{
+//     let name="Nandhini"
+//     console.log(name)
+// }
+// arr_func()
+//Anonymous function
+// let ann_fun=function(){
+//     let name="Nandhini Narayanasamy"
+//    console.log(name)
+// }
+// ann_fun()  
+//IIFE
+(function(param){
+    let name="Nandhini Narayanasamy"
+   console.log(name,param)
+})("is a SOFTWARE ENGINEER")
--- Learning.js
+++
@@ -1,5 +0,0 @@
-//Hoisting
-let acc_name="nandhu";
-console.log(acc_name)
-let bank_name="SBI"
-console.log(bank_name)
